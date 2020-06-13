45046c29773976e14918dce8371fdc7772166193
Manoj Kumar Keerthy <manojkumarkeerthy@gmail.com>
2020-06-13 17:40:10 +0200

    Cascading dropdown for ingredients

--- src/components/ChefComponent/IngredientListRow.js
+++ src/components/ChefComponent/IngredientListRow.js
@@ -5,29 +5,40 @@ import IngredientsService from '../../services/IngredientsService';
 
 const IngredientListRow = ({ingredients}) => {
     const [Ingredients, setIngredients] =useState([]);
+    const [IngredientUnit, setIngredientUnit] = useState("");
+    const [IngredientBrands, setIngredientBrands] = useState([]);
 
     useEffect(() => {
-    IngredientsService.getAll().then((data) => {
-        setIngredients(data);
-    }).catch((e) => {
-        console.error(e);
-    });
-},[]);
+        IngredientsService.getAll().then((data) => {
+            setIngredients(data);
+        }).catch((e) => {
+            console.error(e);
+        });
+    },[]);
+
+    function selectedIngredient(event){
+        console.log(event.target.value);
+        var id = event.target.value;
+        IngredientsService.getIngredient(id).then((data) => {
+            setIngredientUnit(data.parameter);
+            setIngredientBrands(data.brands);
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
+
     return (
         
         <div>
-            <select className="brandDropdown">
+            <select className="brandDropdown" onChange={selectedIngredient}>
                 <option>--Select ingredient--</option>
-                {Ingredients.map(dt =><option>{dt.title} </option>)}
+                {Ingredients.map((dt,i) =><option key={i} value={dt.id}>{dt.title} </option>)}
             </select>
             <input type="number" name="points" step="1" value={ingredients.Amount} className="amountBox"/>
-            <select className="brandDropdown">
-                <option>--Units--</option>
-                {Ingredients.map(dt =><option>{dt.parameter} </option>)}
-            </select>
+            <label>{IngredientUnit}</label>
             <select className="brandDropdown">
                 <option>--Select brand--</option>
-                {Ingredients.map(dt =><option>{dt.brands.brandName} </option>)}
+                {IngredientBrands.map((dt,i) =><option key={i}>{dt.brandName} </option>)}
             </select>
         </div>
     )
