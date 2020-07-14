12a5c94e06dafa44d371b7395885d6034ee4409b
ahmadwardak <ahmad.wardak@tum.de>
2020-07-14 11:16:50 +0200

    IngredientCustomizer - added customization of ingredient amount

--- src/components/RecipeComponent/Ingredient.js
+++ src/components/RecipeComponent/Ingredient.js
@@ -1,78 +1,110 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Recipe.css';
 import '../../App.css';
 import IngredientsService from '../../services/IngredientsService';
 
-const Ingredient = ({id, cartHandler,servingSize, ingredient})=>{   
-    return(
-       <div className='ingredientSection'>
-           <BrandChoice id={id} cartHandler={cartHandler} ingredient={ingredient} servingSize={servingSize}/>
-       </div> 
+import { Row, Col, Form, FormControl, InputGroup, Button, DropdownButton, Dropdown } from 'react-bootstrap';
+
+const Ingredient = ({ id, cartHandler, servingSize, ingredient }) => {
+    return (
+        <div className='ingredientSection'>
+            <BrandChoice id={id} cartHandler={cartHandler} ingredient={ingredient} servingSize={servingSize} />
+        </div>
     )
 }
 
-const BrandChoice = ({id, cartHandler,servingSize,ingredient}) => {
-    function amountChanged(event){
-        cartHandler(event.target.value,id,"amount");
+const BrandChoice = ({ id, cartHandler, servingSize, ingredient }) => {
+    let pointsInput;
+
+    function amountChanged(event) {
+        setAmount(pointsInput.value);
+        console.log(pointsInput.value)
+        cartHandler(pointsInput.value, id, "amount");
+
     }
-    function priceChanged(event){
-        console.log("eventttt",event.target.value);
-        var price=0;
-        if(event.target.value!=0){
-            price=Brands.find(br=> br.brandName == event.target.value).price;
+
+
+    function priceChanged(event) {
+        console.log("eventttt", event.target.value);
+        var price = 0;
+        if (event.target.value != 0) {
+            price = Brands.find(br => br.brandName == event.target.value).price;
             cartHandler(event.target.value, id, "brand");
             cartHandler(true, id, "isActive");
         }
-        else{
+        else {
             cartHandler(ingredient.ingredientBrand, id, "brand");
             cartHandler(false, id, "isActive");
 
         }
-        cartHandler(price,id,"basePrice");
-        cartHandler(price*servingSize,id,"price");
-        
-        
+        cartHandler(price, id, "basePrice");
+        cartHandler(price * servingSize, id, "price");
+
+
         setSelectedBrand(event.target.value);
-        console.log("SELECTED:",selectedBrand);
+        console.log("SELECTED:", selectedBrand);
         setPrice(price);
     }
-    function fixFloat(original){
-        var result = Math.round(original*100)/100;
+    function fixFloat(original) {
+        var result = Math.round(original * 100) / 100;
         return result;
     }
-    const [Brands,setBrands] =useState([]);
-    const [Title, setTitle] =useState("");
+    const [Brands, setBrands] = useState([]);
+    const [Title, setTitle] = useState("");
     const [Parameter, setParameter] = useState("");
     const [Price, setPrice] = useState(0);
-    const [selectedBrand,setSelectedBrand]=useState('');
 
-    var tmp = (ingredient.ingredientQuantity)*servingSize;
+    const [selectedBrand, setSelectedBrand] = useState('');
 
-    const[baseAmount,setBaseAmount]=useState(ingredient.ingredientQuantity);
+    // var tmp = (ingredient.ingredientQuantity) * servingSize;
+    const [Amount, setAmount] = useState(ingredient.ingredientQuantity * servingSize);
+    const [baseAmount, setBaseAmount] = useState(ingredient.ingredientQuantity);
 
     useEffect(() => {
-    IngredientsService.getIngredient(ingredient.ingredientID).then((data) => {
-        setBrands(data.ingredientBrands);
-        setTitle(data.name);
-        setParameter(data.ingredientUnit);
-    }).catch((e) => {
-        console.error(e);
-    });
-},[]);
+        IngredientsService.getIngredient(ingredient.ingredientID).then((data) => {
+            setBrands(data.ingredientBrands);
+            setTitle(data.name);
+            setParameter(data.ingredientUnit);
+        }).catch((e) => {
+            console.error(e);
+        });
+    }, []);
     return (
-        
-        <div>
-            <input type="number" name="points" step="1" value={tmp} onChange={amountChanged} className="amountBox"/>
-            <label className="whiteFont amountParameter">{Parameter}</label>
-            <select className="brandDropdown" onChange={priceChanged} value={selectedBrand}>
-                <option key={0} value={0}>--{Title} from:--</option>
-                { Brands.map(dt => <option key={dt.brandName} value={dt.brandName}>{dt.brandName} </option> )}
-            </select>
-            <div className='priceDiv'>
-                <label className="whiteFont"> {fixFloat((Price* tmp)/baseAmount)} €</label>
-            </div>
-            
-        </div>
+
+        <Row xs="12" className="mb-1">
+            <Col xs="3" className="pl-3 pr-0">
+                <FormControl
+                    aria-label="Ingredient Amount"
+                    defaultValue={Amount}
+                    onChange={amountChanged}
+                    ref={el => { pointsInput = el }}
+                    name="points"
+                    type="number"
+                    min="0"
+                    step="1"
+                />
+            </Col>
+            <Col xs="2" className="pl-3 pr-0 pt-1">
+                <Form.Label style={{ color: "white" }}>
+                    {Parameter}
+                </Form.Label>
+
+            </Col>
+            <Col xs="5" className="pl-3 pr-0">
+                <Form.Control as="select"
+                    onChange={priceChanged}
+                    defaultValue={selectedBrand}  >
+                    <option key={0} value={0}>--{Title} from:--</option>
+                    {Brands.map(dt => <option key={dt.brandName} value={dt.brandName}>{dt.brandName} </option>)}
+                </Form.Control>
+            </Col>
+            <Col xs="2" className="pl-3 pr-0 pt-1">
+                <Form.Label style={{ color: "white" }}>
+                    {fixFloat((Price * Amount) / baseAmount)} €
+                </Form.Label>
+            </Col>
+
+        </Row>
     )
 }
 
--- src/components/RecipeComponent/IngredientCustomizer.js
+++ src/components/RecipeComponent/IngredientCustomizer.js
@@ -2,156 +2,167 @@ import React, { useState, useEffect } from 'react';
 import './Recipe.css';
 import '../../App.css';
 import Ingredient from './Ingredient.js'
-import {  Button } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 
-const IngredientCustomizer = ({servingSize, ingredientsNeeded, addToShoppingCart})=>{
-    const[totalPrice, setTotalPrice]=useState(0.0);
-    const[ServingSize,setServingSize]=useState(servingSize);
-    const[buttonEnabled, setButtonEnabled]=useState(false);
-    
-    var initialItems=[];
-        ingredientsNeeded.map(ing=>{
-            initialItems.push({
-                id:ing.ingredientID, 
-                amount:ing.ingredientQuantity, 
-                price:0, 
-                brand:ing.ingredientBrand,
-                basePrice:0,
-                baseAmount:ing.ingredientQuantity,
-                isActive:false
-            });
+const IngredientCustomizer = ({ servingSize, ingredientsNeeded, addToShoppingCart }) => {
+
+    let servingSizeInput;
+
+    const [totalPrice, setTotalPrice] = useState(0.0);
+    const [ServingSize, setServingSize] = useState(servingSize);
+    const [buttonEnabled, setButtonEnabled] = useState(false);
+
+    var initialItems = [];
+    ingredientsNeeded.map(ing => {
+        initialItems.push({
+            id: ing.ingredientID,
+            amount: ing.ingredientQuantity,
+            price: 0,
+            brand: ing.ingredientBrand,
+            basePrice: 0,
+            baseAmount: ing.ingredientQuantity,
+            isActive: false
         });
-    const[cartItems, setCartItems]=useState(initialItems);
-    
-    
-    function changeServingSize(sSize){
+    });
+    const [cartItems, setCartItems] = useState(initialItems);
+
+
+    function changeServingSize(sSize) {
         var newServingSize = sSize.target.value;
-        console.log("newservs",newServingSize);
+        console.log("newservs", newServingSize);
         setServingSize(newServingSize);
-        cartItems.forEach(element=>{
-            var newPrice= (element.basePrice * newServingSize )/servingSize;
-            newPrice= Math.round(newPrice*100)/100;
-            var newAmount= (newServingSize *element.baseAmount)/servingSize;
-            element.price=newPrice;
-            element.amount=newAmount;
+        cartItems.forEach(element => {
+            var newPrice = (element.basePrice * newServingSize) / servingSize;
+            newPrice = Math.round(newPrice * 100) / 100;
+            var newAmount = (newServingSize * element.baseAmount) / servingSize;
+            element.price = newPrice;
+            element.amount = newAmount;
         });
         calculateTotalPrice();
+        console.log(cartItems)
     }
-    function addToCart(){
-        var finalItems=[];
-        cartItems.map(item=>{
-            if(item.price!=0){
+    function addToCart() {
+        var finalItems = [];
+        cartItems.map(item => {
+            if (item.price != 0) {
                 finalItems.push({
-                    ingredientID:item.id,
-                    ingredientQuantity:item.amount,
-                    ingredientBrand:item.brand,
-                    price:item.price
+                    ingredientID: item.id,
+                    ingredientQuantity: item.amount,
+                    ingredientBrand: item.brand,
+                    price: item.price
                 });
             }
         });
         addToShoppingCart(finalItems, totalPrice);
     }
-    function calculateTotalPrice(){
+    function calculateTotalPrice() {
         var total = 0.0;
-        var newItems= cartItems;
-        newItems.forEach(item =>{
+        var newItems = cartItems;
+        newItems.forEach(item => {
             total = parseFloat(total) + parseFloat(item.price);
         });
         console.log("total", total);
-        setTotalPrice(Math.round(total*100)/100);
+        setTotalPrice(Math.round(total * 100) / 100);
     }
-    function shoppingCartHandler(value,id,type){
-      
-        if(type=="amount"){
-            handleIngredientAmount(value,id);
-        }
-        else if(type=="brand"){
-            handleIngredientBrand(value,id);
-        }
-        else if(type=="price"){
-            handleIngredientPrice(value,id);
+    function shoppingCartHandler(value, id, type) {
+
+        if (type == "amount") {
+            handleIngredientAmount(value, id);
             calculateTotalPrice();
         }
-        else if(type=="basePrice"){
-            handleIngredientBasePrice(value,id);
+        else if (type == "brand") {
+            handleIngredientBrand(value, id);
         }
-        else if(type=="isActive"){
-            handleIngredientActivation(value,id);
+        else if (type == "price") {
+            handleIngredientPrice(value, id);
+            calculateTotalPrice();
+        }
+        else if (type == "basePrice") {
+            handleIngredientBasePrice(value, id);
+        }
+        else if (type == "isActive") {
+            handleIngredientActivation(value, id);
         }
 
-        
+
     }
-    function handleIngredientAmount(value, id){
-        var newItems= cartItems;
-        var itemIndex = newItems.findIndex(it => it.id==id);
+    function handleIngredientAmount(value, id) {
+        var newItems = cartItems;
+        var itemIndex = newItems.findIndex(it => it.id == id);
         newItems[itemIndex].amount = parseInt(value);
+        newItems[itemIndex].price = (newItems[itemIndex].basePrice / servingSizeInput.value) * newItems[itemIndex].amount;
+
         setCartItems(newItems);
+        console.log('aaa', newItems)
     }
-    function handleIngredientBrand(value,id){
-        var newItems= cartItems;
-        var itemIndex = newItems.findIndex(it => it.id==id);
+    function handleIngredientBrand(value, id) {
+        var newItems = cartItems;
+        var itemIndex = newItems.findIndex(it => it.id == id);
         newItems[itemIndex].brand = value;
         setCartItems(newItems);
+        console.log(newItems)
     }
 
-    function handleIngredientPrice(value,id){
-        var newItems= cartItems;
-        var itemIndex = newItems.findIndex(it => it.id==id);
+    function handleIngredientPrice(value, id) {
+        var newItems = cartItems;
+        var itemIndex = newItems.findIndex(it => it.id == id);
         newItems[itemIndex].price = value;
         setCartItems(newItems);
     }
 
-    function handleIngredientBasePrice(value,id){
-        var newItems= cartItems;
-        var itemIndex = newItems.findIndex(it => it.id==id);
+    function handleIngredientBasePrice(value, id) {
+        var newItems = cartItems;
+        var itemIndex = newItems.findIndex(it => it.id == id);
         newItems[itemIndex].basePrice = value;
         setCartItems(newItems);
     }
-    function handleIngredientActivation(value,id){
-        var newItems= cartItems;
-        var itemIndex = newItems.findIndex(it => it.id==id);
+    function handleIngredientActivation(value, id) {
+        var newItems = cartItems;
+        var itemIndex = newItems.findIndex(it => it.id == id);
         newItems[itemIndex].isActive = value;
         setCartItems(newItems);
         handleButton();
     }
-    function handleButton(){
-        var isEnabled=false;
-        cartItems.forEach(item=>{
-            if(item.isActive==true)
-                isEnabled=true;
+    function handleButton() {
+        var isEnabled = false;
+        cartItems.forEach(item => {
+            if (item.isActive == true)
+                isEnabled = true;
         });
         setButtonEnabled(isEnabled);
     }
-    function preventKeyDown(event){
+    function preventKeyDown(event) {
         event.preventDefault();
     }
-    return(
-       <div className='ingredientBox'>
-           <div className='servingSizeDiv'>
+    return (
+        <div className='ingredientBox'>
+            <div className='servingSizeDiv'>
                 <h4 className='whiteFont bigCol'>For how many people you are cooking?</h4>
-                <input type="number" className="servingSizeBox" step="1" min="2" defaultValue={ServingSize} onChange={changeServingSize} onKeyDown={preventKeyDown}></input>
-           </div>
-           <hr className='whiteFont'/>
-           <div className='ingredientChanger'>
-            <h5 className="whiteFont boldFont">Ingredients needed for {ServingSize} people:</h5>
-            <div className="ScrollableContent">
-                { ingredientsNeeded.map(ing => <Ingredient id={ing.ingredientID}  key={ing.ingredientID} cartHandler={shoppingCartHandler} ingredient={ing} servingSize={ServingSize/2}/> )}
+                <input type="number" className="servingSizeBox" step="1" min="2"
+                    ref={el => { servingSizeInput = el }} defaultValue={ServingSize} onChange={changeServingSize} onKeyDown={preventKeyDown}></input>
             </div>
-            <div className="totalPrice">
-                <ul className="totalPriceList">
-                    <li><span className="whiteFont boldFont">Total</span> <span className="whiteFont boldFont">{totalPrice} €</span></li>
-                </ul>
+            <hr className='whiteFont' />
+            <div className='ingredientChanger'>
+                <h5 className="whiteFont boldFont">Ingredients needed for {ServingSize} people:</h5>
+                <div className="ScrollableContent">
+                    {ingredientsNeeded.map(ing => <Ingredient id={ing.ingredientID} key={ing.ingredientID} cartHandler={shoppingCartHandler} ingredient={ing} servingSize={ServingSize / 2} />)}
+
+                </div>
+                <div className="totalPrice">
+                    <ul className="totalPriceList">
+                        <li><span className="whiteFont boldFont">Total</span> <span className="whiteFont boldFont">{totalPrice} €</span></li>
+                    </ul>
+                </div>
+
             </div>
-            
-           </div>
-           <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center">
                 <Button className="btn btn-success btn-block" onClick={addToCart} type="submit" disabled={!buttonEnabled}>
-                            Order
+                    Order
                     </Button>
-           </div>
-           
-       </div> 
+            </div>
+
+        </div>
     )
 }
 
