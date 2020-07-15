1b5373d7fd8959a2be61d0869f7a550006db42a6
ahmadwardak <ahmad.wardak@tum.de>
2020-07-15 14:15:29 +0200

    Recipe Customization Completed
    Code Refactor
    Completed customization of ingredient amount along with customization of Serving Size and Brand

--- src/components/RecipeComponent/Ingredient.js
+++ src/components/RecipeComponent/Ingredient.js
@@ -1,111 +1,119 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 import './Recipe.css';
 import '../../App.css';
 import IngredientsService from '../../services/IngredientsService';
-
 import { Row, Col, Form, FormControl, InputGroup, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 
-const Ingredient = ({ id, cartHandler, servingSize, ingredient }) => {
-    return (
-        <div className='ingredientSection'>
-            <BrandChoice id={id} cartHandler={cartHandler} ingredient={ingredient} servingSize={servingSize} />
-        </div>
-    )
-}
+class Ingredient extends Component {
 
-const BrandChoice = ({ id, cartHandler, servingSize, ingredient }) => {
-    let pointsInput;
-
-    function amountChanged(event) {
-        setAmount(pointsInput.value);
-        console.log(pointsInput.value)
-        cartHandler(pointsInput.value, id, "amount");
-
-    }
-
-
-    function priceChanged(event) {
-        console.log("eventttt", event.target.value);
-        var price = 0;
-        if (event.target.value != 0) {
-            price = Brands.find(br => br.brandName == event.target.value).price;
-            cartHandler(event.target.value, id, "brand");
-            cartHandler(true, id, "isActive");
+    constructor(props) {
+        super(props);
+        this.state = {
+            Title: "",
+            Brands: [],
+            IngredientUnit: ""
         }
-        else {
-            cartHandler(ingredient.ingredientBrand, id, "brand");
-            cartHandler(false, id, "isActive");
 
-        }
-        cartHandler(price, id, "basePrice");
-        cartHandler(price * servingSize, id, "price");
+        this.pointsInput = React.createRef();
+        this.brandSelectInput = React.createRef();
 
+        console.log(this.props);
+        // this.priceChanged = this.priceChanged.bind(this);
+        this.fixFloat = this.fixFloat.bind(this);
 
-        setSelectedBrand(event.target.value);
-        console.log("SELECTED:", selectedBrand);
-        setPrice(price);
     }
-    function fixFloat(original) {
-        var result = Math.round(original * 100) / 100;
-        return result;
-    }
-    const [Brands, setBrands] = useState([]);
-    const [Title, setTitle] = useState("");
-    const [Parameter, setParameter] = useState("");
-    const [Price, setPrice] = useState(0);
 
-    const [selectedBrand, setSelectedBrand] = useState('');
-
-    // var tmp = (ingredient.ingredientQuantity) * servingSize;
-    const [Amount, setAmount] = useState(ingredient.ingredientQuantity * servingSize);
-    const [baseAmount, setBaseAmount] = useState(ingredient.ingredientQuantity);
-
-    useEffect(() => {
-        IngredientsService.getIngredient(ingredient.ingredientID).then((data) => {
-            setBrands(data.ingredientBrands);
-            setTitle(data.name);
-            setParameter(data.ingredientUnit);
+    componentWillMount(props) {
+        IngredientsService.getIngredient(this.props.ingredient.id).then((data) => {
+            this.setState({ Brands: data.ingredientBrands });
+            this.setState({ Title: data.name });
+            this.setState({ IngredientUnit: data.ingredientUnit });
         }).catch((e) => {
             console.error(e);
         });
-    }, []);
-    return (
+    }
 
-        <Row xs="12" className="mb-1">
-            <Col xs="3" className="pl-3 pr-0">
-                <FormControl
-                    aria-label="Ingredient Amount"
-                    defaultValue={Amount}
-                    onChange={amountChanged}
-                    ref={el => { pointsInput = el }}
-                    name="points"
-                    type="number"
-                    min="0"
-                    step="1"
-                />
-            </Col>
-            <Col xs="2" className="pl-3 pr-0 pt-1">
-                <Form.Label style={{ color: "white" }}>
-                    {Parameter}
+    // amountChanged(event) {
+    //     this.setState({ Amount: this.pointsInput.current.value });
+    //     console.log(this.pointsInput.current.value)
+    //     this.props.cartHandler(this.pointsInput.current.value, this.props.id, "amount");
+
+    // }
+
+
+    // priceChanged(event) {
+    //     console.log("eventttt", event.target.value);
+    //     var price = 0;
+    //     if (event.target.value != 0) {
+    //         price = this.state.Brands.find(br => br.brandName == event.target.value).price;
+    //         this.props.cartHandler(event.target.value, this.props.id, "brand");
+    //         this.props.cartHandler(true, this.props.id, "isActive");
+    //     }
+    //     else {
+    //         this.props.cartHandler(this.props.ingredient.ingredientBrand, this.props.id, "brand");
+    //         this.props.cartHandler(false, this.props.id, "isActive");
+
+    //     }
+    //     this.props.cartHandler(price, this.props.id, "basePrice");
+    //     this.props.cartHandler(price * this.props.servingSize, this.props.id, "price");
+
+
+    //     this.setState({ selectedBrand: event.target.value });
+    //     console.log("SELECTED:", event.target.value);
+    //     this.setState({ Price: price });
+    // }
+    fixFloat(original) {
+        if (isNaN(original))
+            return 0;
+
+        var result = Math.round(original * 100) / 100;
+        return result;
+    }
+
+
+    render() {
+        return (
+
+            <Row xs="12" className="mb-1">
+                <Col xs="3" className="pl-3 pr-0">
+                    <FormControl
+                        aria-label="Ingredient Amount"
+                        value={this.props.ingredient.amount}
+                        onChange={() => this.props.onAmountChange(this.pointsInput.current.value, this.props.ingredient.id)}
+                        ref={this.pointsInput}
+                        name="points"
+                        type="number"
+                        min="0"
+                        step="1"
+                    />
+                </Col>
+                <Col xs="2" className="pl-3 pr-0 pt-1">
+                    <Form.Label style={{ color: "white" }}>
+                        {this.state.IngredientUnit}
+                    </Form.Label>
+
+                </Col>
+                <Col xs="5" className="pl-3 pr-0">
+                    <Form.Control as="select"
+                        ref={this.brandSelectInput}
+                        onChange={() => this.props.onBrandChange(this.brandSelectInput.current.value, this.props.ingredient.id)}
+                        defaultValue='0'  >
+                        <option key={0} value={0}>--{this.state.Title} from:--</option>
+                        {this.state.Brands.map(dt => <option key={dt.brandName} value={dt.brandName}>{dt.brandName} </option>)}
+                    </Form.Control>
+                </Col>
+                <Col xs="2" className="pl-3 pr-0 pt-1">
+                    <Form.Label style={{ color: "white" }}>
+                        {this.fixFloat(this.props.ingredient.price)} €
                 </Form.Label>
+                </Col>
 
-            </Col>
-            <Col xs="5" className="pl-3 pr-0">
-                <Form.Control as="select"
-                    onChange={priceChanged}
-                    defaultValue={selectedBrand}  >
-                    <option key={0} value={0}>--{Title} from:--</option>
-                    {Brands.map(dt => <option key={dt.brandName} value={dt.brandName}>{dt.brandName} </option>)}
-                </Form.Control>
-            </Col>
-            <Col xs="2" className="pl-3 pr-0 pt-1">
-                <Form.Label style={{ color: "white" }}>
-                    {fixFloat((Price * Amount) / baseAmount)} €
-                </Form.Label>
-            </Col>
+            </Row>
 
-        </Row>
-    )
+
+        );
+    }
 }
 
-export default Ingredient
+export default withRouter(Ingredient);
