199c06b7161a5797006b743bec5bc1c535696430
Manoj Kumar Keerthy <manojkumarkeerthy@gmail.com>
2020-07-18 11:44:41 +0200

    Update Banner.js

--- src/components/HeaderComponent/Banner.js
+++ src/components/HeaderComponent/Banner.js
@@ -48,7 +48,7 @@ class Banner extends Component {
                                             <div className=" justify-content-center" style={{ display: 'flex', flexWrap: 'wrap' }}>
                                                 <div className="item">eChef is a one-stop go-to platform to identify ingredients </div>
                                                 <div style={{ flexBasis: '100%', height: '0' }}> </div>
-                                                <div className="item" >of recipe and buy in a single click.</div>
+                                                <div className="item" >of a recipe and buy them in a single click.</div>
                                                 <div style={{ flexBasis: '100%', height: '10px' }}> </div>
                                                 <div className="item" >Learn more <Link to='/about'>about us</Link>.</div>
                                             </div>
